b4437c7cac964ee493c374fc9c7129878fac40d3
Sujeet Kumar <Sujeet.Kumar@ADP.com>
2019-09-20 22:01:16 +0530

    Input tags adding done

--- src/script/input-tags-component.js
+++ src/script/input-tags-component.js
@@ -25,120 +25,83 @@ class InputTagsWebComponent extends LitElement {
   render() {
     return html`
       <style>
-        .autocom:focus{
-            outline: none;
-        }
-        .autocom-container{
-          border-bottom: 1px solid #4a4a4a;
-          line-height: 1.8em;
-        }
-        .autocom{
-             width: 100%;
-             height: 25px;
-             margin: 0;
-             border-top: 0;
-             border-left: 0;
-             border-right: 0;
-             border-bottom: 0;
-             border-style: solid;
-             padding: 0;
-             font-size: 14px;
-             color: #4a4a4a;
-             line-height: 1.8px;
-             border-color: #4a4a4a;
-        }
-        .autocom-container ul{
-            padding: 0px;
-        }
-        .autocom-container ul li{
-            list-style: none;
-            padding: 15px;
-            cursor: pointer;
-        }
-        .autocom-container ul li:hover{
-            background: #1a8099;
-            color: #ffffff;
-        }
-        .drop-down-box{
-             box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
-             max-height: 250px;
-             overflow: auto;
-             position: absolute;
-             background: #ffffff;
-             right: 0;
-             left: 0;
-             width: inherit;
-        }
-        #close-icon {
-          color: #4a4a4a;
-          font-family: 'Helvetica', 'Arial', sans-serif;
-          text-align: right;
-          width: 20px;
-          height: 20px;
-          border-radius: 1px;
-          position: absolute;
-          cursor: pointer;
-          left:400px
-      }
-      .drop-down-box{
-        display: none;
-        margin-left: 10px;
-      }
       #tags {
         max-width: 600px;
-        display:flex;
+        border-bottom: 1px solid #4a4a4a;
+        padding: 5px;
+        padding-left:0px;
     }
     #tags .tag {
-        position: relative;
-        display: block;
+        display: flex;
         float: left;
         color: #FFFFFF;
-        background: #8cce0c;
-        padding: 5px 20px 5px 5px;
-        border-radius: 2px;
-        font-size: 12px;
+        background: gray;
+        padding: 4px 22px 6px 8px;
+        border-radius: 10px;
         line-height: 12px;
-        font-family: "Lato Heavy";
         transition: all 0.3s ease-in-out;
+        margin-right: 5px;
+        margin-top: 1px;
     }
     #tags .tag .close {
-        position: absolute;
-        top: 0;
-        right: 0;
-        width: 14px;
-        height: 100%;
-        background: #75ac0a;
+        background: #1a8099;
         cursor: pointer;
-        border-radius: 0 2px 2px 0;
+        border-radius: 50%;
         transition: background 0.3s;
+        margin-right: -15px;
+        margin-left: 5px;
     }
     #tags .tag .close:after {
-        position: absolute;
         content: "×";
-        top: 6px;
-        left: 3px;
         font-weight: 900;
+        float: right;
     }
+    #add_tag{
+      outline: none;
+      border: none;
+    }
+    .autocom-container ul{
+      padding: 0px;
+  }
+  .autocom-container ul li{
+      list-style: none;
+      padding: 15px;
+      cursor: pointer;
+  }
+  .autocom-container ul li:hover{
+      background: #1a8099;
+      color: #ffffff;
+  }
+  .drop-down-box{
+       box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
+       max-height: 250px;
+       overflow: auto;
+       position: absolute;
+       background: #ffffff;
+       right: 0;
+       left: 0;
+       width: inherit;
+       display: none;
+        margin-left: 10px;
+  }
       </style>
       <div class="autocom-container" style="width: ${this.width}px">
-       ${this.isSet ? html`<div id="close-icon" @click=${this.clearField}>
-         <span id="x">X</span>
-      </div>` : ''}
       <div id="tags">
         <div id="tag-wrapper">
-           
+          
         </div>
+        <div id="input-wrapper">
+          <input type="text" id="add_tag" 
+          placeholder=${this.placeholder} .value="${this.selectedItem}" @keyup=${this.filter}>
+        </div>
+      </div>
       
-      <div id="input-wrapper">
-         <input name="autocom" placeholder=${this.placeholder} .value="${this.selectedItem}" class="autocom" type="text"  @keyup=${this.filter}>
-      </div>
-      </div>
-      <div class="drop-down-box" id="list-container">
+       <div class="drop-down-box" id="list-container">
          <ul id="list">
             ${this.searchResult.map((item) => html`<li @click=${this.selectItem.bind(this, item)}>${item['name']}</li>`)}
          </ul>
-       </div>
-       </div>`
+       </div></div>`;
+     
   }
   /*
    * TO Select item from search list
@@ -149,7 +112,7 @@ class InputTagsWebComponent extends LitElement {
     this.isSet = true;
     this.shadowRoot.getElementById('list-container').style.display = 'none';
     this.shadowRoot.getElementById('tag-wrapper')
-    .insertAdjacentHTML('beforeend', '<span class="tag">'+item.name+'<span class="close"></span></span>');
+    .insertAdjacentHTML('beforeend', '<div class="tag"><div>'+item.name+'</div><div class="close"></div><div>');
  // --- TO retun value to parent component ---//
     this.dispatchEvent(new CustomEvent('on-change', {
       detail: item
