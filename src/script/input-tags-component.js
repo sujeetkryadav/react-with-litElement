63bb92a5ab3a056f021127505a333c23d9fb33b6
Sujeet Kumar <Sujeet.Kumar@ADP.com>
2019-09-25 17:24:54 +0530

    External styling done

--- src/script/input-tags-component.js
+++ src/script/input-tags-component.js
@@ -1,40 +1,38 @@
-import {LitElement, html} from 'lit-element';
-
+import { LitElement, html } from 'lit-element';
 class InputTagsWebComponent extends LitElement {
-  
-  static get properties() {
-    return {
-      searchResult: {type: []} ,
-      selectedItem: {type: String},
-      isSelected: {type: Boolean},
-      isSet: {type: Boolean},
-      url: {type: String},
-      width: {type: Number},
-      placeholder: {type: String},
-      userList: {type: []},
-      removeUser: {type: Function}
-    };
-  }
-
-  constructor() {
-    super();
-    this.searchResult  = [];
-    this.selectedItem = '';
-    this.isSelected = false;
-    this.width = 200;
-    this.isSet = false;
-    this.userList = [];
-  }
-  render() {
-    return html`
+    constructor() {
+        super();
+        this.searchResult = [];
+        this.selectedItem = '';
+        this.isSelected = false;
+        this.width = 200;
+        this.isSet = false;
+        this.userList = [];
+        this.url = undefined;
+    }
+    createRenderRoot() { return this;}
+    static get properties() {
+        return {
+            searchResult: { type: [] },
+            selectedItem: { type: String },
+            isSelected: { type: Boolean },
+            isSet: { type: Boolean },
+            url: { type: String },
+            width: { type: Number },
+            placeholder: { type: String },
+            userList: { type: [] },
+        };
+    }
+    render() {
+        return html `
       <style>
-      #tags {
+      input-tags-component #tags {
         max-width: 600px;
         border-bottom: 1px solid #4a4a4a;
         padding: 5px;
         padding-left:0px;
     }
-    #tags .tag {
+    input-tags-component #tags .tag {
         display: flex;
         float: left;
         color: #FFFFFF;
@@ -46,7 +44,7 @@ class InputTagsWebComponent extends LitElement {
         margin-right: 5px;
         margin-top: 1px;
     }
-    #tags .tag .close {
+    input-tags-component #tags .tag .close {
         background: #1a8099;
         cursor: pointer;
         border-radius: 50%;
@@ -54,28 +52,38 @@ class InputTagsWebComponent extends LitElement {
         margin-right: -15px;
         margin-left: 5px;
     }
-    #tags .tag .close:after {
+    input-tags-component #tags .tag .close:after {
         content: "×";
         font-weight: 900;
         float: right;
     }
-    #add_tag{
+    input-tags-component #add_tag{
       outline: none;
       border: none;
     }
-    .autocom-container ul{
+    input-tags-component .autocom-container-tags ul{
       padding: 0px;
   }
-  .autocom-container ul li{
+  input-tags-component .autocom-container-tags ul li{
       list-style: none;
       padding: 15px;
       cursor: pointer;
   }
-  .autocom-container ul li:hover{
+  input-tags-component .autocom-container-tags ul li:hover{
       background: #1a8099;
       color: #ffffff;
   }
-  .drop-down-box{
+  input-tags-component #close-icon {
+          color: #4a4a4a;
+          font-family: 'Helvetica', 'Arial', sans-serif;
+          text-align: right;
+          width: 20px;
+          height: 20px;
+          border-radius: 1px;
+          position: absolute;
+          cursor: pointer;
+      }
+      input-tags-component .autocom-container-tags .drop-down-box{
        box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
        max-height: 250px;
        overflow: auto;
@@ -86,12 +94,14 @@ class InputTagsWebComponent extends LitElement {
        width: inherit;
        display: none;
         margin-left: 10px;
+        margin-top: 0; 
   }
+   
       </style>
-      <div class="autocom-container" style="width: ${this.width}px">
+      <div class="autocom-container-tags" style="width: ${this.width}px">
       <div id="tags">
         <div id="tag-wrapper">
-        ${this.userList.map((item) => html`<div class="tag"><div>${item['name']}</div><div class="close" @click=${this.removeUser.bind(this, item)}></div><div>`)}
+        ${this.userList.map((item) => html `<div class="tag"><div>${item['name']}</div><div class="close" @click=${this.removeUser.bind(this, item)}></div><div>`)}
         </div>
         <div id="input-wrapper">
           <input type="text" id="add_tag" 
@@ -99,83 +109,82 @@ class InputTagsWebComponent extends LitElement {
         </div>
       </div>
       
-       <div class="drop-down-box" id="list-container">
+       <div class="drop-down-box" id="list-container-tags">
          <ul id="list">
-            ${this.searchResult.map((item) => html`<li @click=${this.selectItem.bind(this, item)}>${item['name']}</li>`)}
+            ${this.searchResult.map((item) => html `<li @click=${this.selectItem.bind(this, item)}>${item['name']}</li>`)}
          </ul>
        </div></div>`;
-     
-  }
-  /*
-   * TO Select item from search list
-   */
-  selectItem(item) {
-    this.selectedItem = null;
-    this.isSelected = false;
-    this.isSet = true;
-    this.shadowRoot.getElementById('list-container').style.display = 'none';
-    if(this.userList.indexOf(item) === -1){
-      this.userList.push(item);
-      // this.shadowRoot.getElementById('tag-wrapper')
-      // .insertAdjacentHTML('beforeend', '<div class="tag"><div>'+item.name+'</div><div class="close"></div><div>');
     }
- // --- TO retun value to parent component ---//
-    this.dispatchEvent(new CustomEvent('on-change', {
-      detail: this.userList
-    }));
-  }
-  /*
-   * TO Clear selected field
-   */
-  clearField($event) {
-    this.selectedItem = '';
-    this.isSet = false;
-  }
-  /*
-   * To Detect change in property
-   */
-  updated(changedProperties) {
-    changedProperties.forEach((oldValue, propName) => {
-      if(propName === 'url' && (this.url !== undefined && this.url !== 'undefined')){
-        this.loadData(this.url);
-      }
-    });
-   
-  }
- 
-  loadData(url) {
-    const self = this;
-    const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', url , true);
-    xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
-    xhttp.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        self.searchResult = JSON.parse(this.response);
-      }
-    };
-    xhttp.send();
-  }
-
-  filter($event){
-    this.isSelected = true;
-      this.shadowRoot.getElementById('list-container').style.display = 'block';
-      this.shadowRoot.querySelectorAll('#list li').forEach((val, index) => {
-        const txtValue = val.textContent;
-        if (txtValue.toUpperCase().indexOf($event.target.value.toUpperCase()) > -1) {
-          val['style'].display = 'block';
-        } else {
-          val['style'].display = 'none';
+    /*
+     * TO Select item from search list
+     */
+    selectItem(item) {
+        this.selectedItem = null;
+        this.isSelected = false;
+        this.isSet = true;
+        document.getElementById('list-container-tags').style.display = 'none';
+        if (this.userList.indexOf(item) === -1) {
+            this.userList.push(item);
+            // this.shadowRoot.getElementById('tag-wrapper')
+            // .insertAdjacentHTML('beforeend', '<div class="tag"><div>'+item.name+'</div><div class="close"></div><div>');
         }
-      });
-  }
-  removeUser(item){
-    const indexToDelete = this.userList.indexOf(item);
-    this.userList = this.userList.filter(
-      (toDelete, index) => index !== indexToDelete
-    );
-     this.dispatchEvent(new CustomEvent('on-change', {
-      detail: this.userList
-     }));
-  }
+        // --- TO retun value to parent component ---//
+        this.dispatchEvent(new CustomEvent('on-change', {
+            detail: this.userList
+        }));
+    }
+    /*
+     * TO Clear selected field
+     */
+    clearField($event) {
+        this.selectedItem = '';
+        this.isSet = false;
+        this.userList = [];
+        this.dispatchEvent(new CustomEvent('on-change', {
+            detail: this.userList
+        }));
+    }
+    /*
+     * To Detect change in property
+     */
+    updated(changedProperties) {
+        changedProperties.forEach((oldValue, propName) => {
+            if (propName === 'url' && (this.url !== undefined && this.url !== 'undefined')) {
+                this.loadData(this.url);
+            }
+        });
+    }
+    loadData(url) {
+        const self = this;
+        const xhttp = new XMLHttpRequest();
+        xhttp.open('GET', url, true);
+        xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                self.searchResult = JSON.parse(this.response);
+            }
+        };
+        xhttp.send();
+    }
+    filter($event) {
+        this.isSelected = true;
+        document.getElementById('list-container-tags').style.display = 'block';
+        document.querySelectorAll('#list li').forEach((val, index) => {
+            const txtValue = val.textContent;
+            if (txtValue.toUpperCase().indexOf($event.target.value.toUpperCase()) > -1) {
+                val['style'].display = 'block';
+            }
+            else {
+                val['style'].display = 'none';
+            }
+        });
+    }
+    removeUser(item) {
+        const indexToDelete = this.userList.indexOf(item);
+        this.userList = this.userList.filter((toDelete, index) => index !== indexToDelete);
+        this.dispatchEvent(new CustomEvent('on-change', {
+            detail: this.userList
+        }));
+    }
 }
 customElements.define('input-tags-component', InputTagsWebComponent);
